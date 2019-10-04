102aa5670ea8ba54aa534010a302ebe1307a4ed9
KelvinYang0320 <kelvin777320@gmail.com>
2019-10-04 17:28:57 +0800

    7 types of blocks

--- block.cpp
+++ block.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include<string>
 using namespace std;
 class block{
     public:
+        friend class Blocks;
+        block():row(0),col(0){};
         block(int i, int j):row(i),col(j){};
         void change_row(int r_change){
             row = r_change;
@@ -19,10 +22,208 @@ class block{
         int row;
         int col;
 };
+class Blocks{
+    public:
+        Blocks(){
+            smArray = nullptr;
+        }
+        Blocks(char type, int rot){
+            smArray = new block [4];
+            switch(type){
+                case 'T':
+                    switch(rot){
+                        case 1:
+                            smArray[0]=block(0,1);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(1,2);
+                            break;
+                        case 2:
+                            smArray[0]=block(0,1);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(2,1);
+                            break;
+                        case 3:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(0,1);
+                            smArray[2]=block(0,2);
+                            smArray[3]=block(1,1);
+                            break;
+                        case 4:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(2,0);
+                            break;
+                        default:
+                            cout<<"[T]Unrecognized Type"<<endl;
+                            break;
+                    }
+                    break;
+                case 'L':
+                    switch(rot){
+                        case 1:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(0,1);
+                            smArray[2]=block(1,0);
+                            smArray[3]=block(2,0);
+                            break;
+                        case 2:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(1,2);
+                            break;
+                        case 3:
+                            smArray[0]=block(0,1);
+                            smArray[1]=block(1,1);
+                            smArray[2]=block(2,0);
+                            smArray[3]=block(2,1);
+                            break;
+                        case 4:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(0,1);
+                            smArray[2]=block(0,2);
+                            smArray[3]=block(1,2);
+                            break;
+                        default:
+                            cout<<"[L]Unrecognized Type"<<endl;
+                            break;
+                    }
+                    break;
+                case 'J':
+                    switch(rot){
+                        case 1:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(0,1);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(2,1);
+                            break;
+                        case 2:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(0,1);
+                            smArray[2]=block(0,2);
+                            smArray[3]=block(1,0);
+                            break;
+                        case 3:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(2,0);
+                            smArray[3]=block(2,1);
+                            break;
+                        case 4:
+                            smArray[0]=block(0,2);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(1,2);
+                            break;
+                        default:
+                            cout<<"[J]Unrecognized Type"<<endl;
+                            break;
+                    }
+                    break;
+                case 'S':
+                    switch(rot){
+                        case 1:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(0,1);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(1,2);
+                            break;
+                        case 2:
+                            smArray[0]=block(0,1);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(2,0);
+                            break;
+                        default:
+                            cout<<"[S]Unrecognized Type"<<endl;
+                            break;
+                    }
+                    break;
+                case 'Z':
+                    switch(rot){
+                        case 1:
+                            smArray[0]=block(0,1);
+                            smArray[1]=block(0,2);
+                            smArray[2]=block(1,0);
+                            smArray[3]=block(1,1);
+                            break;
+                        case 2:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(1,1);
+                            smArray[3]=block(2,1);
+                            break;
+                        default:
+                            cout<<"[Z]Unrecognized Type"<<endl;
+                            break;
+                    }
+                    break;
+                case 'I':
+                    switch(rot){
+                        case 1:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(1,0);
+                            smArray[2]=block(2,0);
+                            smArray[3]=block(3,0);
+                            break;
+                        case 2:
+                            smArray[0]=block(0,0);
+                            smArray[1]=block(0,1);
+                            smArray[2]=block(0,2);
+                            smArray[3]=block(0,3);
+                            break;
+                        default:
+                            cout<<"[I]Unrecognized Type"<<endl;
+                            break;
+                    }
+                    break;
+                case 'O':
+                    smArray[0]=block(0,0);
+                    smArray[1]=block(0,1);
+                    smArray[2]=block(1,0);
+                    smArray[3]=block(1,1);
+                    break;
+                default:
+                    cout<<"Unrecognized Type"<<endl;
+                    break;
+            }
+        }
+        void show_all_pos(void){
+            for(int i=0;i<4;i++){
+                cout<<"r:"<<smArray[i].row<<"\tc:"<<smArray[i].col<<endl;
+            }
+        }
+    private:
+        block *smArray;
+};
 int main(void)
 {
+    freopen("test.txt","r",stdin);
+    int Map_r, Map_c;
     int row, col;
-    cin>>row>>col;
-    block B(row, col);
-    cout<<B.show_row()<<B.show_col();
+    char block_type;
+    int block_rot;
+    int drop_point;
+    Blocks *tmp;
+    cin>>Map_r>>Map_c;
+    cout<<"Map Size:"<<Map_r<<"*"<<Map_c<<endl;
+    while(1){
+        cin>>block_type;
+        if(block_type == 'E') break;
+        else if(block_type == 'T' || block_type == 'L' ||
+                block_type == 'J' || block_type == 'S' || 
+                block_type == 'Z' || block_type == 'I') cin>>block_rot;
+        else if(block_type != 'O'){
+            cout<<"[Error]"<<block_type<<endl;
+            break;
+        }
+        tmp = new Blocks(block_type, block_rot);
+        cin>>drop_point;
+        cout<<block_type<<block_rot<<" "<<drop_point<<endl;
+        tmp->show_all_pos();
+    }
+    return 0;
 }
